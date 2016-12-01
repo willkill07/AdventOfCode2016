68d808aa26e6ab9f69afaf136430acd4412d188a
Will Killian <william.killian@gmail.com>
2016-12-01 18:37:52 -0500

    [Day01] askalski (@Voltara) bit hacks integrated

--- src/Day01.cpp
+++ src/Day01.cpp
@@ -7,20 +7,17 @@ void
 solve<Day01>(bool part2, std::istream& is, std::ostream& os)
 {
   std::set<std::pair<int, int>> visited;
-  int  index{1}, x{0}, y{0}, dist;
+  int  index{1}, pos[2]{0, 0}, dist;
   char d;
   for (; is >> d >> dist; is.ignore(1, ',')) {
-    // wrap-around turning; implicit direction order is E,N,W,S
-    index = (index + 4 + ((d == 'R') ? -1 : 1)) % 4;
-    int  sign = (index & 0b10) ? -1 : 1;
-    int& face = (index & 0b01) ? y : x;
+    index += ~d & 3;
     for (int i{0}; i < dist; ++i) {
-      face += sign;
-      if (part2 && !visited.emplace(x, y).second) {
-        os << (std::abs(x) + std::abs(y)) << std::endl;
+      pos[index & 1] += 1 - (index & 2);
+      if (part2 && !visited.emplace(pos[0], pos[1]).second) {
+        os << (std::abs(pos[0]) + std::abs(pos[1])) << std::endl;
         return;
       }
     }
   }
-  os << (std::abs(x) + std::abs(y)) << std::endl;
+  os << (std::abs(pos[0]) + std::abs(pos[1])) << std::endl;
 }
