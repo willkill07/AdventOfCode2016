f30ee939e3c3b0137cc29566d460fc5adf6a7e63
Will Killian <william.killian@gmail.com>
2016-12-03 12:20:27 -0500

    [Day03] Initial commit

--- src/Day03.cpp
+++ src/Day03.cpp
@@ -1,11 +1,26 @@
 #include "Solution.hpp"
-#include "io.hpp"
+#include <algorithm>
+#include <iterator>
+#include <vector>
+
+bool
+isTri(int a, int b, int c)
+{
+  int sum{a + b + c}, max{std::max(a, std::max(b, c))};
+  return sum - max > max;
+}
 
 template <>
 void
 solve<Day03>(bool part2, std::istream& is, std::ostream& os)
 {
-  if (part2) {
-    os << io::as_string(is);
+  int num{part2 ? 3 : 1}, sum{0};
+  std::vector<int> tri(3 * num);
+  for (std::istream_iterator<int> itr{is}, end{}; itr != end;) {
+    for (int& t : tri)
+      t = *itr++;
+    for (int n{0}; n < num; ++n)
+      sum += isTri(tri[n], tri[n + num], tri[n + 2 * num]);
   }
+  os << sum << std::endl;
 }
