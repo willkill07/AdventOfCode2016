6ff02bdb2c83fd1c74ee38437564ae693e9deb2d
Will Killian <william.killian@gmail.com>
2016-12-06 00:23:21 -0500

    [Day06] Initial commit

--- src/Day06.cpp
+++ src/Day06.cpp
@@ -1,11 +1,17 @@
 #include "Solution.hpp"
-#include "io.hpp"
+#include <array>
 
 template <>
 void
-solve<Day06>(bool part2, std::istream& is, std::ostream& os)
+solve<Day06>(bool part2, std::istream &is, std::ostream &os)
 {
-  if (part2) {
-    os << io::as_string(is);
-  }
+  std::array<std::array<int, 26>, 8> counts;
+  std::string line;
+  while (std::getline(is, line))
+    for (uint i{0}; i < line.size(); ++i)
+      ++counts[i][line[i] - 'a'];
+  auto cmp{part2 ? std::min_element<const int *> : std::max_element<const int *>};
+  for (const auto &c : counts)
+    os << char(cmp(c.begin(), c.end()) - c.begin() + 'a');
+  os << std::endl;
 }
