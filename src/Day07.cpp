4cd9ca5c3f00916deae57ee08e379285612c90c6
Will Killian <william.killian@gmail.com>
2016-12-07 10:28:10 -0500

    [Day07] Initial commit

--- src/Day07.cpp
+++ src/Day07.cpp
@@ -1,11 +1,39 @@
 #include "Solution.hpp"
 #include "io.hpp"
+#include <numeric>
+#include <set>
+
+int
+abba(int val, const std::string& s)
+{
+  bool out{false}, in{false}, hyper{false};
+  for (uint i{0}; i < s.size() - 3; ++i) {
+    hyper ^= (s[i] < 'a');
+    if (s[i] == s[i + 3] && s[i + 1] == s[i + 2] && s[i] != s[i + 1])
+      in |= hyper, out |= !hyper;
+  }
+  return val + (out && !in);
+}
+
+int
+aba(int val, const std::string& s)
+{
+  std::set<std::pair<char, char>> lookup[2];
+  bool hyper{false};
+  for (uint i{0}; i < s.size() - 2; ++i) {
+    hyper ^= (s[i] < 'a');
+    if (s[i] == s[i + 2] && s[i] != s[i + 1])
+      lookup[hyper].emplace(s[i + hyper], s[i + !hyper]);
+  }
+  for (auto ab : lookup[0])
+    if (lookup[1].find(ab) != lookup[1].end())
+      return val + 1;
+  return val;
+}
 
 template <>
 void
 solve<Day07>(bool part2, std::istream& is, std::ostream& os)
 {
-  if (part2) {
-    os << io::as_string(is);
-  }
+  os << std::accumulate(io::as<io::line>(is), {}, 0, (part2 ? aba : abba)) << std::endl;
 }
