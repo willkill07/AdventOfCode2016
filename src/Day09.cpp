018071495c7bc5320b4ac33c0d803a821c9bf521
Will Killian <william.killian@gmail.com>
2016-12-09 12:01:55 -0500

    [Day09] Initial commit

--- src/Day09.cpp
+++ src/Day09.cpp
@@ -1,11 +1,21 @@
 #include "Solution.hpp"
 #include "io.hpp"
 
+const static std::regex PATTERN{R"(\((\d+)x(\d+)\))", std::regex::optimize};
+
+long
+decode(std::string s, bool part2)
+{
+  std::smatch m;
+  if (!std::regex_search(s, m, PATTERN))
+    return s.size();
+  long start{m.position()}, len{std::stoi(m.str(1))}, times{std::stoi(m.str(2))}, ptr{start + m.length()};
+  return start + (part2 ? decode(s.substr(ptr, len), part2) : len) * times + decode(s.substr(ptr + len), part2);
+}
+
 template <>
 void
 solve<Day09>(bool part2, std::istream& is, std::ostream& os)
 {
-  if (part2) {
-    os << io::as_string(is);
-  }
+  os << (decode(io::as_string(is), part2) - 1) << std::endl;
 }
