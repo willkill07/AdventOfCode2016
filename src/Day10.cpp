d31328b78ef062f29fed0386ee2293dfb6764c9f
Will Killian <william.killian@gmail.com>
2016-12-10 18:25:29 -0500

    [Day10] Switched to deferred lambdas

--- src/Day10.cpp
+++ src/Day10.cpp
@@ -1,56 +1,65 @@
 #include "Solution.hpp"
 #include "io.hpp"
-#include <chrono>
 #include <map>
 #include <set>
-#include <thread>
 #include <vector>
-using namespace std::chrono_literals;
+
+class Data {
+  std::set<int> vals{};
+public:
+  int insert(int v) {
+    vals.insert(v);
+    return v;
+  }
+  bool ready() const {
+    return vals.size() == 2;
+  }
+  int min() const {
+    return *vals.begin();
+  }
+  int max() const {
+    return *vals.rbegin();
+  }
+};
+
+const static std::regex VALUE{R"(value (\d+) goes to bot (\d+))"};
+const static std::regex BOT{R"(bot (\d+) gives low to (\w+) (\d+) and high to (\w+) (\d+))"};
 
 template <>
 void
 solve<Day10>(bool part2, std::istream& is, std::ostream& os)
 {
-  const static std::regex VALUE{R"(value (\d+) goes to bot (\d+))", std::regex::optimize};
-  const static std::regex BOT{R"(bot (\d+) gives low to (\w+) (\d+) and high to (\w+) (\d+))", std::regex::optimize};
-  std::vector<std::pair<int, int>> values;
-  std::vector<std::thread> bots;
+  std::map<std::string, std::map<int, Data>> data;
+  std::vector<std::function<bool()>> bots;
   std::map<int, int> ids;
-  std::map<std::string,std::map<int, std::set<int>>> data;
-  std::mutex lock;
-  std::smatch match;
-  for (const auto& line : io::by<io::line>(is))
+  for (const auto& line : io::by<io::line>(is)) {
+    std::smatch m;
+    std::regex_match(line, m, VALUE) || std::regex_match(line, m, BOT);
     if (line[0] == 'v') {
-      std::regex_match(line, match, VALUE);
-      std::lock_guard<std::mutex> guard(lock);
-      int val{std::stoi(match.str(1))}, bot{std::stoi(match.str(2))};
-      data["bot"][bot].insert(val);
+      int bot{std::stoi(m.str(2))}, v{std::stoi(m.str(1))};
+      auto& d = data["bot"][bot];
+      d.insert(v);
     } else {
-      std::regex_match(line, match, BOT);
-      int bot{std::stoi(match.str(1))}, v1{std::stoi(match.str(3))}, v2{std::stoi(match.str(5))};
-      std::string t1{match.str(2)}, t2{match.str(4)};
+      int bot{std::stoi(m.str(1))}, v1{std::stoi(m.str(3))}, v2{std::stoi(m.str(5))};
+      std::string t1{m.str(2)}, t2{m.str(4)};
       ids.emplace(bot, bots.size());
-      bots.emplace_back([bot,v1,v2,t1,t2,part2,&data,&os,&lock]() {
-        while (true) {
-          std::this_thread::sleep_for(10us);
-          std::lock_guard<std::mutex> guard(lock);
-          size_t size{data["bot"][bot].size()};
-          if (size != 2)
-            continue;
-          auto ptr = data["bot"][bot].begin();
-          int low{*ptr}, high{*++ptr};
-          if (!part2 && (low == 17 && high == 61))
-            os << bot << std::endl;
-          data[t1][v1].insert(low);
-          data[t2][v2].insert(high);
-          break;
-        }
+      bots.push_back([bot, v1, v2, t1, t2, &data]() {
+        const auto& d = data["bot"][bot];
+        return ((data[t1][v1].insert(d.min()) ^ 17) + (data[t2][v2].insert(d.max()) ^ 61) == 0);
       });
     }
-  for (auto& b : bots)
-    b.join();
-  if (part2) {
-    auto get = [&] (int i) { return *data["output"][i].begin(); };
-    os << (get(0) * get(1) * get(2)) << std::endl;
   }
+  std::set<int> queue;
+  for (const auto& id : ids)
+    queue.insert(id.first);
+  while (!queue.empty())
+    for (auto q = queue.begin(); q != queue.end();)
+      if (data["bot"][*q].ready()) {
+        if (bots[ids[*q]]() && !part2)
+          os << *q << std::endl;
+        queue.erase(q++);
+      } else
+        ++q;
+  if (part2)
+    os << (data["output"][0].min() * data["output"][1].min() * data["output"][2].min()) << std::endl;
 }
