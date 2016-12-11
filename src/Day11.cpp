af7173283617a191c35fc994af5812443c75ce34
Will Killian <william.killian@gmail.com>
2016-12-11 14:41:20 -0500

    [Day11] Shortened code a bit

--- src/Day11.cpp
+++ src/Day11.cpp
@@ -2,19 +2,14 @@
 #include "io.hpp"
 
 // I am aware this solution only works for certain inputs
-
 const static std::regex ITEM{R"(a (\w+)(?:-compatible (microchip)| (generator)))"};
 
 template <>
 void
 solve<Day11>(bool part2, std::istream& is, std::ostream& os)
 {
-  std::vector<int> floors;
-  for (std::string line; std::getline(is,line); )
-    floors.push_back(std::distance(std::sregex_iterator(line.begin(), line.end(), ITEM), {}));
-  floors[0] += part2 * 4;
-  int result{-9};
-  for (int i{0}; i < 4; ++i)
-    result += floors[i] * (3 - i) * 2;
-  os << result << std::endl;
+  int floorFactor{3}, sum{part2 ? 15 : -9};
+  for (auto line : io::by<io::line>(is))
+    sum += 2 * floorFactor-- * std::distance(std::sregex_iterator(line.begin(), line.end(), ITEM), {});
+  os << sum << std::endl;
 }
