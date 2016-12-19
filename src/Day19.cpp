dcf08450d16ca425668512589d81694bb3ea2c1e
Will Killian <william.killian@gmail.com>
2016-12-19 16:05:07 -0500

    [Day19] Golfin' around

--- src/Day19.cpp
+++ src/Day19.cpp
@@ -1,16 +1,11 @@
 #include "Solution.hpp"
+#include <cmath>
 
 template <>
 void
 solve<Day19>(bool part2, std::istream& is, std::ostream& os)
 {
-  int elfCount{std::stoi(std::string{std::istream_iterator<char>(is), {}})};
-  int result{part2 ? 1 : __builtin_clz(elfCount)};
-  if (part2) {
-    while ((result * 3) <= elfCount)
-      result *= 3;
-    result = elfCount - result;
-  } else
-    result = (elfCount << (result + 1) >> result | 1);
-  os << result << std::endl;
+  int elves{*std::istream_iterator<int>(is)};
+  int r{part2 ? int(std::pow(3, int((std::log2(elves - 1) / std::log2(3))))) :  __builtin_clz(elves)};
+  os << (part2 ? elves - r + std::max(elves - 2 * r, 0) : elves << (r + 1) >> r | 1) << std::endl;
 }
