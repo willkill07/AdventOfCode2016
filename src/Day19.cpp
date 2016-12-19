a82db94827e93b2a5ca07ce409cb799cfaf066c1
Will Killian <william.killian@gmail.com>
2016-12-19 01:58:46 -0500

    [Day19] Initial commit

--- src/Day19.cpp
+++ src/Day19.cpp
@@ -1,11 +1,16 @@
 #include "Solution.hpp"
-#include "io.hpp"
 
 template <>
 void
 solve<Day19>(bool part2, std::istream& is, std::ostream& os)
 {
+  int elfCount{std::stoi(std::string{std::istream_iterator<char>(is), {}})};
+  int result{part2 ? 1 : __builtin_clz(elfCount)};
   if (part2) {
-    os << io::as_string(is);
-  }
+    while ((result * 3) <= elfCount)
+      result *= 3;
+    result = elfCount - result;
+  } else
+    result = (elfCount << (result + 1) >> result | 1);
+  os << result << std::endl;
 }
