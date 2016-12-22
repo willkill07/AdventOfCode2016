cc96071039c66c034b04f6c1ff367838b1133713
Will Killian <william.killian@gmail.com>
2016-12-22 13:54:22 -0500

    [Day14] Now working in parallel mode

--- src/Day14.cpp
+++ src/Day14.cpp
@@ -9,24 +9,28 @@
 #include <thread>
 #include <vector>
 
+struct LSet {
+  std::set<int> v;
+  std::mutex    m;
+  void
+  put(int val)
+  {
+    std::lock_guard<std::mutex> g{m};
+    v.emplace(val);
+  }
+};
+
+const std::string LOOKUP{"0123456789abcdef"};
+const std::regex  R3{R"((.)\1\1)", std::regex::optimize}, R5{R"((.)\1\1\1\1)", std::regex::optimize};
+const int         GOAL{64}, WINDOW{1000}, LIMIT{32768};
+
 void
 key(const char* input, size_t len, bool part2, md5str_t* digest)
 {
   md5str((uint8_t*)input, len, digest);
-  if (part2)
-    for (int i{0}; i < 2016; ++i)
-      md5str((uint8_t*)&(digest->c[0]), 32, digest);
+  for (int i{0}; part2 && i < 2016; ++i)
+    md5str((uint8_t*)digest, 32, digest);
 }
-
-const int         GOAL{64}, WINDOW{1000}, LIMIT{25600};
-const std::string LOOKUP{"0123456789abcdef"};
-const std::regex  R3{R"((.)\1\1)", std::regex::optimize};
-const std::regex  R5{R"((.)\1\1\1\1)", std::regex::optimize};
-
-struct LSet {
-  std::set<int> v;
-  std::mutex m;
-};
 
 template <>
 void
@@ -38,34 +42,31 @@ solve<Day14>(bool part2, std::istream& is, std::ostream& os)
   std::string      in;
   is >> in;
 
-  for (uint tid{0}; tid < std::thread::hardware_concurrency(); ++tid) {
-    threads.emplace_back([&] (uint tid) {
+  for (uint32_t tid{0}; tid < std::thread::hardware_concurrency(); ++tid)
+    threads.emplace_back([&](uint tid) {
       std::string input{in};
-      md5str_t buf;
-      uint length(input.size());
+      md5str_t    buf;
+      uint32_t    length(input.size());
       input.resize(length + 10);
       for (uint val{tid}; val < LIMIT; val += std::thread::hardware_concurrency()) {
         key(input.data(), length + util::fast_itoa(val, &input[length]), part2, &buf);
         const std::string h{buf.begin(), buf.end()};
-        for (std::sregex_iterator ri{h.begin(), h.end(), R5}, re{}; ri != re; ++ri) {
-          auto& curr = l5[LOOKUP.find(ri->str(1)[0])];
-          curr.m.lock(), curr.v.emplace(val), curr.m.unlock();
-        }
+        for (std::sregex_iterator ri{h.begin(), h.end(), R5}, re{}; ri != re; ++ri)
+          l5[LOOKUP.find(ri->str(1)[0])].put(val);
         std::smatch m;
-        if (std::regex_search(h, m, R3)) {
-          auto& curr = l3[LOOKUP.find(m.str(1)[0])];
-          curr.m.lock(), curr.v.emplace(val), curr.m.unlock();
-        }
+        if (std::regex_search(h, m, R3))
+          l3[LOOKUP.find(m.str(1)[0])].put(val);
       }
     }, tid);
-  }
   for (auto& t : threads)
     t.join();
-  for (uint i{0}; i < 16; ++i)
-    for (auto i5 : l5[i].v)
-      for (auto i3 : l3[i].v)
-        if (i3 < i5 && i3 + WINDOW >= i5)
+  for (int i{0}; i < 16; ++i)
+    for (auto i3 : l3[i].v)
+      for (auto i5 : l5[i].v)
+        if (i3 < i5 && i5 - i3 < WINDOW) {
           ind.emplace_back(i3);
+          break;
+        }
   std::sort(ind.begin(), ind.end());
   os << ind[GOAL - 1] << '\t' << std::endl;
 }
