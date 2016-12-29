e1236b8df71da773e3e47bddc5bae6f99a01b61b
Will Killian <william.killian@gmail.com>
2016-12-29 17:46:00 -0500

    [Day09] Make it fast (no regex, no copying)

--- src/Day09.cpp
+++ src/Day09.cpp
@@ -2,19 +2,27 @@
 #include "io.hpp"
 
 long
-decode(std::string && s, bool part2)
+decode(std::string::const_iterator s0, std::string::const_iterator s1, bool part2)
 {
-  static const std::regex PATTERN{R"(\((\d+)x(\d+)\))", std::regex::optimize};
-  std::smatch m;
-  if (!std::regex_search(s, m, PATTERN))
-    return s.size();
-  long start{m.position()}, len{std::stoi(m.str(1))}, times{std::stoi(m.str(2))}, ptr{start + m.length()};
-  return start + (part2 ? decode(s.substr(ptr, len), part2) : len) * times + decode(s.substr(ptr + len), part2);
+  auto i1 = s0;
+  while (i1 != s1 && *i1 != '(')
+    ++i1;
+  if (i1 == s1)
+    return std::distance(s0, s1);
+  auto i0 = i1;
+  int  l{0}, r{0};
+  while (*++i1 != 'x')
+    l *= 10, l += (*i1 - '0');
+  while (*++i1 != ')')
+    r *= 10, r += (*i1 - '0');
+  auto s = ++i1 + l;
+  return std::distance(s0, i0) + r * (part2 ? decode(i1, s, true) : std::distance(i1, s)) + decode(s, s1, part2);
 }
 
 template <>
 void
 solve<Day09>(bool part2, std::istream& is, std::ostream& os)
 {
-  os << (decode(io::as_string(is), part2) - 1) << std::endl;
+  std::string input{io::as_string(is)};
+  os << (decode(input.begin(), input.end(), part2) - 1) << std::endl;
 }
