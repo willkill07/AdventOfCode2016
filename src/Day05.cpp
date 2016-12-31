fdc00db8ec7cda0e9f77b6c2413f3eb2b841b059
Will Killian <william.killian@gmail.com>
2016-12-31 10:24:40 -0500

    [Day05] Remove string construction and extra branching

--- src/Day05.cpp
+++ src/Day05.cpp
@@ -1,7 +1,7 @@
 #include "Solution.hpp"
-#include "io.hpp"
 #include "md5.hpp"
 #include "util.hpp"
+#include <algorithm>
 
 const static std::string LOOKUP{"0123456789abcdef"};
 
@@ -9,26 +9,26 @@ template <>
 void
 solve<Day05>(bool part2, std::istream& is, std::ostream& os)
 {
-  std::string password{"________"}, input{io::as_string(is)};
-  int         numDone{0};
-  util::parallel_do([part2, &numDone, &password, &os](int groupIdx, int groupSize, std::string input) {
+  std::string password{"________"}, input;
+  int         numDone{0}, inserted[16];
+  std::getline(is, input);
+  std::fill(std::begin(inserted), std::end(inserted), -1);
+  util::parallel_do([&](int groupIdx, int groupSize, std::string input) {
     md5str_t buf;
     int      length(input.size()), id{groupIdx};
     input.reserve(input.size() + 10);
-    while (numDone != 8) {
-      int num{-1}, pos{part2 ? -1 : numDone};
+    for (; numDone != 8; id += groupSize) {
+      int num{-1}, pos{numDone};
       md5str((uint8_t*)input.data(), length + util::fast_itoa(id, &input[length]), &buf);
-      if (std::string{buf.begin(), buf.begin() + 5} == "00000") {
-        num = util::htoi(buf.c[5]);
-        if (part2) {
-          pos = num, num = util::htoi(buf.c[6]);
-          if (pos > 7 || password[pos] != '_' || password.find(LOOKUP[num]) != std::string::npos)
-            num = -1;
-        }
-        if (num != -1)
-          password[pos] = LOOKUP[num], ++numDone;
+      if (std::count(buf.cbegin(), buf.cbegin() + 5, '0') != 5)
+        continue;
+      num = util::htoi(buf.c[5]);
+      if (part2) {
+        pos = num, num = util::htoi(buf.c[6]);
+        if (pos > 7 || password[pos] != '_' || inserted[num] != -1)
+          continue;
       }
-      id += groupSize;
+      password[pos] = LOOKUP[num], ++numDone, inserted[num] = pos;
     }
   }, input);
   os << password << std::endl;
