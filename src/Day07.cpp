5972de64436dfbe51edf46b0da80f8d8cacfda98
Will Killian <william.killian@gmail.com>
2017-01-02 20:24:55 -0500

    [Day07] Improved logic to save 0.2ms

--- src/Day07.cpp
+++ src/Day07.cpp
@@ -1,35 +1,36 @@
 #include "Solution.hpp"
-#include "io.hpp"
-#include <numeric>
 #include <set>
 
-int
-abba(int val, const std::string& s)
+bool
+abba(const std::string& s)
 {
   bool out{false}, in{false}, hyper{false};
   for (uint i{0}; i < s.size() - 3; ++i)
-    if (hyper ^= (s[i] < 'a'), !((s[i] ^ s[i + 3]) + (s[i + 1] ^ s[i + 2])) && (s[i] ^ s[i + 1]))
+    if (hyper ^= (s[i] < 'a'), s[i] != s[i + 1] && s[i + 1] == s[i + 2] && s[i] == s[i + 3])
       in |= hyper, out |= !hyper;
-  return val + (out && !in);
+  return out && !in;
 }
 
-int
-aba(int val, const std::string& s)
+bool
+aba(const std::string& s)
 {
   std::set<std::pair<char, char>> lookup[2];
   bool hyper{false};
   for (uint i{0}; i < s.size() - 2; ++i)
-    if (hyper ^= (s[i] < 'a'), !(s[i] ^ s[i + 2]) && (s[i] ^ s[i + 1]))
+    if (hyper ^= (s[i] < 'a'), s[i] == s[i + 2] && s[i] != s[i + 1])
       lookup[hyper].emplace(s[i + hyper], s[i + !hyper]);
-  for (auto ab : lookup[0])
+  for (const auto &ab : lookup[0])
     if (lookup[1].find(ab) != lookup[1].end())
-      return val + 1;
-  return val;
+      return true;
+  return false;
 }
 
 template <>
 void
 solve<Day07>(bool part2, std::istream& is, std::ostream& os)
 {
-  os << std::accumulate(io::as<io::line>(is), {}, 0, (part2 ? aba : abba)) << std::endl;
+  int sum{0};
+  for (std::string line; std::getline(is, line); )
+    sum += (part2 ? aba: abba)(line);
+  os << sum << std::endl;
 }
