095e985b437b12d531b573f7d75f0018ed399d67
Will Killian <william.killian@gmail.com>
2017-01-03 13:53:03 -0500

    [Day22] Remove regex

--- src/Day22.cpp
+++ src/Day22.cpp
@@ -1,6 +1,5 @@
 #include "Solution.hpp"
 #include <array>
-#include <regex>
 
 struct Node {
   int size, used, avail;
@@ -12,19 +11,20 @@ solve<Day22>(bool part2, std::istream& is, std::ostream& os)
 {
   static const int WIDTH{37}, HEIGHT{25};
   std::array<std::array<Node,WIDTH>,HEIGHT> nodes;
-  static const std::regex FS{R"(^/dev/grid/node-x(\d+)-y(\d+)\s+(\d+)T\s+(\d+)T\s+(\d+)T\s+\d+%$)", std::regex::optimize};
   int pairs{0}, emptyX{0}, emptyY{0};
-  std::smatch match;
-  for (std::string line; std::getline(is, line); )
-    if (std::regex_match(line, match, FS))
-      nodes[std::stoi(match.str(2))][std::stoi(match.str(1))] = {std::stoi(match.str(3)), std::stoi(match.str(4)), std::stoi(match.str(5))};
+  for (std::string line; std::getline(is, line); ) {
+    Node n;
+    int x, y;
+    if (sscanf(line.c_str(), "/dev/grid/node-x%d-y%d %dT %dT %dT %*d%%", &x, &y, &n.size, &n.used, &n.avail))
+      nodes[y][x] = n;
+  }
   Node* n = &nodes[0][0];
   for (int n1{0}; n1 < WIDTH * HEIGHT; ++n1)
     if(part2) {
       if (n[n1].used == 0)
         emptyX = (n1 % WIDTH), emptyY = (n1 / WIDTH);
     } else
-      for (int n2{0}; !part2 && n2 < WIDTH * HEIGHT; ++n2)
+      for (int n2{0}; n2 < WIDTH * HEIGHT; ++n2)
         pairs += ((n1 != n2) && (n[n1].used != 0) && (n[n1].used <= n[n2].avail));
   os << (part2 ? ((emptyX + emptyY + WIDTH - 1) + (WIDTH - 2) * 5) : pairs) << std::endl;
 }
