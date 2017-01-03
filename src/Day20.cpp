c0a15f8d13737aa02bc6f2cff5da873c54864f63
Will Killian <william.killian@gmail.com>
2017-01-03 13:44:56 -0500

    [Day20] Remove regex

--- src/Day20.cpp
+++ src/Day20.cpp
@@ -1,22 +1,22 @@
 #include "Solution.hpp"
-#include "io.hpp"
 #include <set>
+#include <utility>
+#include <vector>
 
 template <>
 void
 solve<Day20>(bool part2, std::istream& is, std::ostream& os)
 {
-  std::vector<std::pair<int64_t,int64_t>> data;
-  const std::regex PARSE{R"((\d+)-(\d+))"};
-  for (std::string line; std::getline(is, line); ) {
-    std::smatch match;
-    if (std::regex_match(line, match, PARSE))
-      data.emplace_back(std::stoull(match.str(1)), std::stoull(match.str(2)));
+  std::vector<std::pair<int64_t, int64_t>> data;
+  for (std::string line; std::getline(is, line);) {
+    uint64_t a, b;
+    sscanf(line.c_str(), "%llu-%llu", &a, &b);
+    data.emplace_back(a, b);
   }
   std::sort(data.begin(), data.end());
   int64_t MAX{4294967295}, total{0}, last{-1}, first{-1};
-  for (const auto &d : data) {
-    int64_t c {std::max(int64_t{0}, d.first - last - 1)};
+  for (const auto& d : data) {
+    int64_t c{std::max(int64_t{0}, d.first - last - 1)};
     if (!part2 && first == -1 && c)
       first = last + 1;
     if (part2)
